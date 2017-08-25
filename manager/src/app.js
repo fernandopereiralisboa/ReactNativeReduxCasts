a771cb337e6973e327b6873c1b50ec24814e41a7
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2017-08-25 17:43:46 -0300

    Created Router

--- manager/src/app.js
+++ manager/src/app.js
@@ -5,6 +5,7 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 import firebaseConfig from '../firebase.config.json';
 
@@ -27,7 +28,7 @@ class App extends Component {
 
         return(
             <Provider store={store}>
-                <LoginForm />
+                <Router />
             </Provider>
         )
     }
